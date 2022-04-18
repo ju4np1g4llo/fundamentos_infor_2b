a570a0a9b70cff971136b326a2eacd0ececf081c
ju4np1g4llo <juanpigallo01@gmail.com>
2022-04-18 16:28:50 -0300

    PRACTICA 1 PYTHON INTRO

--- intro_python/ejs_pyint1.py
+++ intro_python/ejs_pyint1.py
@@ -53,9 +53,27 @@ def horas():
 #Realizar un programa que devuelva el dinero que recibirá por comisión luego de 4 ventas y el total de dinero que ganará ese mes, 
 #teniendo en cuenta estas ventas y su sueldo base.
 
+def sueldo():
+    sueldo_base = int(input("Escriba su sueldo base (en numeros): ")) 
+    venta1 = int(input("1ra venta: "))
+    venta2 = int(input("2da venta: "))
+    venta3 = int(input("3ra venta: "))
+    venta4 = int(input("4ta venta: "))
+    total = venta1 * 0.1 + venta2 * 0.1 + venta3 * 0.1 + venta4 *0.1
+    return total + sueldo_base
+
 ##### **Ejercicio 8**
 #Escribir un programa para calcular la nota final de un estudiante, teniendo en cuenta que por cada respuesta correcta el estudiante suma 4 puntos, 
 #por cada incorrecta se le resta 1 punto y si la respuesta está en blanco no se le suma ni se le resta.
 
-
+def examen():
+    correctas = int(input("Escriba el numero de respuestas correctas del alumno: "))
+    incorrectas = int(input("Escriba el numero de respuestas incorrectas del alumno: "))
+    blancas = int(input("Escriba el numero de respuestas vacias del alumno: "))
+    preguntas = int(input("Escriba el numero de preguntas del examen: "))
+    posible = preguntas * 4
+    puntos = correctas * 4 + incorrectas * -1 + blancas * 0
+    print(puntos)
+    print(posible)
+    return puntos / posible * 10
 
