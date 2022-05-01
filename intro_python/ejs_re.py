d4e0f3482cf724037a63de1b3f43dfa2c192453a
ju4np1g4llo <juanpigallo01@gmail.com>
2022-05-01 19:03:33 -0300

    Update ejs_re.py

--- intro_python/ejs_re.py
+++ intro_python/ejs_re.py
@@ -1,16 +1,30 @@
 # **Expresiones Regulares**
 
 ##### **Ejercicio 1**
-Escribí un programa que verifique si un string tiene al menos un carácter permitido. Estos caracteres son a-z, A-Z y 0-9.
+
+from ast import Not
+import re
+
+def verificar(string):
+    if re.search(r"[a-zA-Z0-9]", string):
+        print("Tiene al menos uno de los caracteres")
+    else:
+        print("No tiene al menos uno de los caracteres")
 
 ##### **Ejercicio 2**
 
-Escribí un programa que verifique si un string tiene todos sus caracteres permitidos. Estos caracteres son a-z, A-Z y 0-9.
+import re
+
+def verificar(string):
+    if re.search(r"\W", string):
+        print("Tiene algun caracter no permitido")
+    else:
+        print("Tiene todos los caracteres permitidos")
 
 ##### **Ejercicio 3**
-Creá un programa que verifique las siguientes condiciones:
+#Creá un programa que verifique las siguientes condiciones:
     
-* si un string dado tiene una _h_ seguida de ninguna o más _e_.
+#* si un string dado tiene una _h_ seguida de ninguna o más _e_.
 import re 
 
 def encontrar_patron(string):
@@ -20,7 +34,7 @@ def encontrar_patron(string):
     else:
         return "No se encontro el patron"
         
-* si un string dado tiene una _h_ seguida de una o más _e_.
+#* si un string dado tiene una _h_ seguida de una o más _e_.
 import re
 
 def encontrar_patron(string):
@@ -29,9 +43,9 @@ def encontrar_patron(string):
         return "Se encontro el patron"
     else:
         return "No se encontro el patron"
-* si un string dado tiene una _h_ seguida de una o más _e_.
-
-* si un string dado tiene una _h_ seguida de dos o tres _e_.
+#* si un string dado tiene una _h_ seguida de una o más _e_.
+#esta repetido
+#* si un string dado tiene una _h_ seguida de dos o tres _e_.
 import re
 
 def encontrar_patron(string):
@@ -41,51 +55,68 @@ def encontrar_patron(string):
     else:
         return "No se encontro el patron"
 
+##### **Ejercicio 4*
+import re
 
-
-
-
-##### **Ejercicio 4**
-Realizá un programa que encuentre una palabra unida a otra con un guión bajo en un string dado (el string no debe contener espacios).
+def unida(string):
+    return re.findall(r"(\w+)_(\w+)", string)
 
 ##### **Ejercicio 5**
-Escribí un programa que diga si un string empieza con un número específico.
+#Escribí un programa que diga si un string empieza con un número específico.
+
+def empieza(string,numero):
+    print(bool(re.search("^"+str(numero),string)))
 
 ##### **Ejercicio 6**
-Escribí un programa que dada una lista de strings verifique si se encuentran en una frase dada.
+#Escribí un programa que dada una lista de strings verifique si se encuentran en una frase dada.
+
+def verLista(string):
+    aparecen = []
+    lista = ["Habia","una","vez","algo"]
+    for i in lista:
+        if re.search(i,string) is not None:
+            aparecen.append(i)
+        else:
+            None
+    print(aparecen)
 
 ##### **Ejercicio 7**
 Realizá un programa que encuentre un string que contenga solamente letras minúsculas, mayúsculas, espacios y números.
 
+def minMayEspN(string):
+    if re.search(r"([^\w\s]+)",string):
+       print("Tiene algun caracter no valido")
+    else:
+        print("El string esta bien")            #creo que no pide esto
+
 ##### **Ejercicio 8**
-Escribí un programa que separe y devuelva los caracteres númericos de un string.
+#Escribí un programa que separe y devuelva los caracteres númericos de un string.
+
+def numeros_en_string(string):
+    print(re.findall("[0-9]",string))
 
 ##### **Ejercicio 9**
-Escribí un programa que extraiga los caracteres que estén entre guiones en un string. (String de ejemplo: _"Hoy estuvimos trabajando con re -regular expression- en python -con VSCode-"_)
+#Escribí un programa que extraiga los caracteres que estén entre guiones en un string. (String de ejemplo: _"Hoy estuvimos trabajando con re -regular expression- en python -con VSCode-"_)
 
 import re
 
 def entre_guiones(string):
-    return re.findall(r"-(.*?)-", string) #el ? favorece los matches internos, el * dice que aparezca 0 o mas veces
+    return re.findall(r"-(.*?)-", string)
 
 string = "Hoy estuvimos trabajando con re -regular expression- en python -con VSCode-"
 
 print(entre_guiones(string))
 
-
-
-
-
 ##### **Ejercicio 10**
-Obtené las substrings y las posiciones de estas en una string dada considerando que las substrings están delimitadas por los caracteres _@_ o _&_.
+#Obtené las substrings y las posiciones de estas en una string dada considerando que las substrings están delimitadas por los caracteres _@_ o _&_.
 
+def substrings(string):
+    return re.findall(r"[@&](.*?)[@&]",string)
 
-
-
-# r"-([\w+_-]@[\w+]\.[a-z]{2-6})-"     patron que puede buscar mails
-# esta entre parentesis para buscar un mail que este entre guiones            
+# r"-([\w+_-]@[\w+]\.[a-z]{2-6})-"     patron que puede buscar mails visto en clase practica    
+       
 ##### **Ejercicio 11**
-Realizá un programa que dado una lista de strings verifique que dos palabras dentro del string empiecen con la letra P y las imprima. (Lista de ejemplo: _["Práctica Python", "Práctica C++", "Práctica Fortran"]_).
+#Realizá un programa que dado una lista de strings verifique que dos palabras dentro del string empiecen con la letra P y las imprima. (Lista de ejemplo: _["Práctica Python", "Práctica C++", "Práctica Fortran"]_).
 import re
 
 def dos_p(lista):
@@ -94,16 +125,27 @@ def dos_p(lista):
         if resultado is None:
             print(resultado.group())
 
-lista = 
 
 ##### **Ejercicio 12**
-Escribí un programa que reemplace todas las ocurrencias de espacios, guiones bajos y dos puntos por la barra vertical (**|**).
+#Escribí un programa que reemplace todas las ocurrencias de espacios, guiones bajos y dos puntos por la barra vertical (**|**).
+
+def reemplazar(string):
+        return re.sub(r"[:_\s]","|",string)
 
 ##### **Ejercicio 13**
-Escribí un programa que reemplace los dos primeros caracteres no alfanúmericos por guiones bajos.
+#Escribí un programa que reemplace los dos primeros caracteres no alfanúmericos por guiones bajos.
+
+def dos_no_alfa(string):
+    print(re.sub(r"\W","_",string,2))
 
 ##### **Ejercicio 14**
-Realizá un programa que reemplace los espacios y tabulaciones por punto y coma.
+#Realizá un programa que reemplace los espacios y tabulaciones por punto y coma.
+
+def reemplazar2(string):
+        return re.sub(r"[\t\s]",";",string)
 
 ##### **Ejercicio 15**
-Realizá un programa que validar si una cuenta de mail está escrita correctamente.
+#Realizá un programa que validar si una cuenta de mail está escrita correctamente.
+
+def mail(mail):
+    print(bool(re.match(r"(\S+)@(\w+).(\w+)",mail)))                                                           
